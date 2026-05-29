29f3085f143dea53cb127fe87e9fba85ae4b5490
DMCLEMORE98776 <DMCLEMORE98776@UAT.EDU>
2026-05-28 22:45:14 -0500

    Code update 5/28/26
    Final code update

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TypeVibe2026Form
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            BTNmusic = new Button();
+            BTNsave = new Button();
+            BTNopenfile = new Button();
+            RTBeditor = new RichTextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Plum;
+            panel1.Controls.Add(BTNmusic);
+            panel1.Controls.Add(BTNsave);
+            panel1.Controls.Add(BTNopenfile);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(835, 51);
+            panel1.TabIndex = 0;
+            // 
+            // BTNmusic
+            // 
+            BTNmusic.BackColor = Color.Indigo;
+            BTNmusic.FlatStyle = FlatStyle.Popup;
+            BTNmusic.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic);
+            BTNmusic.ForeColor = SystemColors.ButtonHighlight;
+            BTNmusic.Location = new Point(174, 12);
+            BTNmusic.Name = "BTNmusic";
+            BTNmusic.Size = new Size(75, 23);
+            BTNmusic.TabIndex = 3;
+            BTNmusic.Text = "_PlayMusic";
+            BTNmusic.UseVisualStyleBackColor = false;
+            BTNmusic.Click += BTNmusic_Click;
+            // 
+            // BTNsave
+            // 
+            BTNsave.BackColor = Color.Indigo;
+            BTNsave.FlatStyle = FlatStyle.Popup;
+            BTNsave.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic);
+            BTNsave.ForeColor = SystemColors.ButtonHighlight;
+            BTNsave.Location = new Point(93, 12);
+            BTNsave.Name = "BTNsave";
+            BTNsave.Size = new Size(75, 23);
+            BTNsave.TabIndex = 2;
+            BTNsave.Text = "_SaveFile";
+            BTNsave.UseVisualStyleBackColor = false;
+            BTNsave.Click += BTNsave_Click;
+            // 
+            // BTNopenfile
+            // 
+            BTNopenfile.BackColor = Color.Indigo;
+            BTNopenfile.FlatStyle = FlatStyle.Popup;
+            BTNopenfile.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic);
+            BTNopenfile.ForeColor = SystemColors.ButtonHighlight;
+            BTNopenfile.Location = new Point(12, 12);
+            BTNopenfile.Name = "BTNopenfile";
+            BTNopenfile.Size = new Size(75, 23);
+            BTNopenfile.TabIndex = 1;
+            BTNopenfile.Text = "_OpenFile";
+            BTNopenfile.UseVisualStyleBackColor = false;
+            BTNopenfile.Click += BTNopenfile_Click;
+            // 
+            // RTBeditor
+            // 
+            RTBeditor.BackColor = Color.FromArgb(64, 0, 64);
+            RTBeditor.Font = new Font("Palatino Linotype", 14.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            RTBeditor.ForeColor = SystemColors.ButtonHighlight;
+            RTBeditor.Location = new Point(110, 227);
+            RTBeditor.Name = "RTBeditor";
+            RTBeditor.Size = new Size(611, 159);
+            RTBeditor.TabIndex = 1;
+            RTBeditor.Text = "";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackgroundImage = Properties.Resources.TypeVibeBG;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(835, 450);
+            Controls.Add(RTBeditor);
+            Controls.Add(panel1);
+            Name = "MainForm";
             Text = "Form1";
+            Load += MainForm_Load;
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button BTNsave;
+        private Button BTNopenfile;
+        private Button BTNmusic;
+        private RichTextBox RTBeditor;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,10 +1,88 @@
+using System.Media;
+using System.Web;
+
 namespace TypeVibe2026Form
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTNopenfile_Click(object sender, EventArgs e)
+        {
+            // Lets create an open dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //lets show user a search window
+            DialogResult userChoice = openFileDialog.ShowDialog();
+
+            //did our user click ok yet?
+            if(userChoice == DialogResult.OK)
+            {
+                //lets get the file path
+                string chosenfilePath = openFileDialog.FileName;
+
+                //lets read the file
+                StreamReader streamReader = new StreamReader(chosenfilePath);
+
+                //read the file contents
+                string fileContents = streamReader.ReadToEnd();
+
+                //Add the file contents to our editor RTB
+                RTBeditor.Text = fileContents;
+
+                //close the stream reader so that the file does not get corrupted
+                streamReader.Close();
+            }
+            
+        }
+
+        private void BTNsave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // show save window to our user
+            DialogResult userChoice = saveFileDialog.ShowDialog();
+
+            // Did our user click ok to confirm a save?
+            if (userChoice == DialogResult.OK)
+            {
+                //lets get the file path
+                string saveLocation = saveFileDialog.FileName;
+
+                //lets create a stream writer to write to the file
+                StreamWriter streamWriter = new StreamWriter(saveLocation);
+
+                // Save the text from our editor
+                streamWriter.Write(RTBeditor.Text);
+
+                //close the stream writer so that the file does not get corrupted
+                streamWriter.Close();
+            }
+        }
+
+        private void BTNmusic_Click(object sender, EventArgs e)
+        {
+
+            // locate where our program is living
+            string path = Path.Combine(Application.StartupPath, "rainsound.wav");
+
+            // This shows where the path of the music file is located
+            MessageBox.Show(path);
+
+            // Create a sound player object
+            SoundPlayer player = new SoundPlayer(path);
+            player.Play();
+
+
+            
         }
     }
 }
